da5544834395d8c1236d9d916c082305cfde10f7
brianabaker <brianaclairbaker@gmail.com>
2018-01-09 13:14:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative 'config/environment'
 
+require 'pry'
+
 class App < Sinatra::Base
 
   get '/food_form' do
@@ -7,5 +9,14 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+
+ # post is the form method
+ # the form throws the data to the server
+ # and it gets caught by having the same receiving address and a way of receiving the data
+ # the name attributes of the form input correspond to the key in the params hash for that data
+
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 
 end
